f0da6255c5cb1e7f1d57c5db2065556b2ebefb28
Jiewu Deng <jiewu9293@gmail.com>
2026-09-22 21:26:24 +0800

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        #farthest index we can reach
+        farthest = 0
+        for i, jump_length in enumerate(nums):
+            ## If the current index is beyond farthest,
+            # we cannot reach this position.
+            if i > farthest:
+                return False
+
+            farthest = max(farthest, i + jump_length)
+
+            # If we can reach or pass the last index, return early.
+            if farthest >= len(nums) -1:
+                return True
+        return True
+
